990e91d84f2a04d92f4f880276736f29022dc759
Saranya Wenger <wengers2@bzz.ch>
2023-06-13 11:18:28 +0200

    read esl-files

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import os
 import xml.etree.ElementTree as ET
+from collections import defaultdict
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -12,40 +13,51 @@ def hello_world():  # put application's code here
 
 @app.route('/zaehlerstaende')
 def zaehlerstaende():
-    tag_values = read_esl_files()
+    values = process_xml_files("C:/Users/Saranya Wenger/PycharmProjects/M306/files/ESL-Files")
     test = "tests"
-    return render_template('zaehlerstaende.html', values=tag_values, test=test)
+    return render_template('zaehlerstaende.html', values=values, test=test)
 
 
-def read_esl_files():
-    count = 0
-    tag_values = {}
-    folder_path = "C:/Users/Saranya Wenger/PycharmProjects/M306/files/ESL-Files"
-    bezug = ["1-1:1.8.1", "1-1:1.8.2"]
-    for file_name in os.listdir(folder_path):  # loop through all files
-        if file_name.endswith(".xml"):
-            file_path = os.path.join(folder_path, file_name)
-            file_data = []
-            try:
-                tree = ET.parse(file_path)
-                root = tree.getroot()
-                for value_row in root.findall(".//ValueRow"):  # through all rows
-                    obis = value_row.attrib.get("obis")
-                    if obis in bezug:
-                        data = {}
-                        tag_value = float(value_row.attrib.get("value"))
-                        data['timestamp'] = value_row.attrib.get("valueTimeStamp", None)
-                        if count == 0:
-                            value1 = tag_value
-                            count += 1
-                        else:
-                            tag_value = value1 + tag_value
-                            data['value'].append(tag_value)
-                            count = 0
-            except ET.ParseError:
-                print(f"Error parsing XML file: {file_path}")
+def process_xml_files(directory):
+    # Create a dictionary to store the results
+    result = defaultdict(lambda: {"Bezug": 0, "Einspeisung": 0})
 
-    return data
+    # A set to store the unique timestamps
+    seen_timestamps = set()
+
+    # Iterate over all files in the directory
+    for filename in os.listdir(directory):
+        # Check if the current file is an XML file
+        if filename.endswith('.xml'):
+            # Parse the XML file
+            tree = ET.parse(os.path.join(directory, filename))
+            root = tree.getroot()
+
+            # Iterate over all 'TimePeriod' elements in the XML file
+            for time_period in root.iter('TimePeriod'):
+                # Get the timestamp
+                timestamp = time_period.get('end')
+
+                # Check if we have already processed this timestamp
+                if timestamp in seen_timestamps:
+                    continue
+
+                # Add the timestamp to the set of seen timestamps
+                seen_timestamps.add(timestamp)
+
+                # Iterate over all 'ValueRow' elements in the 'TimePeriod'
+                for value_row in time_period.iter('ValueRow'):
+                    # Check the 'obis' attribute and add the value to the correct category
+                    obis = value_row.get('obis')
+                    value = float(value_row.get('value'))
+
+                    if obis in ["1-1:1.8.1", "1-1:1.8.2"]:
+                        result[timestamp]["Bezug"] += value
+                    elif obis in ["1-1:2.8.1", "1-1:2.8.2"]:
+                        result[timestamp]["Einspeisung"] += value
+
+    # Convert the defaultdict back into a dict
+    return dict(result)
 
 
 if __name__ == '__main__':
